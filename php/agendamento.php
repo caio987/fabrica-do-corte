be7f9cd149439a0f446ec73eba98d610972f2bb2
Ewerton <ewertonhsenai@gmail.com>
2025-10-28 17:01:30 -0300

    Atualizando a página de agendamento(enviando ids para o PHP)

--- php/agendamento.php
+++ php/agendamento.php
@@ -1,5 +1,33 @@
 <?php
     require_once 'config.php';
-    
- echo $_SESSION ['teste'];
+    session_start();
+    $input = json_decode(file_get_contents('php://input'), true);
+    $idTipo = $_SESSION['tipo'];
+    //ids    
+    //id_cliente
+    $idCliente = $_SESSION['id'];
+    //id_funcionario
+    $idFuncionario = $input['id_funcionario'];
+    //id_disponibilidade
+    $idDisponibilidade = $_SESSION ['id_disponibilidade'];
+    //id_servico
+    $idServico = $input['id_servico'];
+
+         if ($_SESSION['tipo'] == 'Cliente') {
+             try {
+                $consulta = $pdo->prepare("INSERT INTO agendamento id_cliente, id_funcionario, id_disponibilidade VALUES (:id_cliente, :id_funcionario, :id_disponibilidade)");
+             } catch (\Throwable $th) {
+                //throw $th;
+             }
+             
+             
+             
+             
+             //Agendamento_servico
+     
+             //id_agendamento
+    }else{
+        echo 'TEste';
+
+    }
 ?>
